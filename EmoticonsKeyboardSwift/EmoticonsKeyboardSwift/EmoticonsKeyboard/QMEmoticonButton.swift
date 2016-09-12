0d3d9a1824623cd96661b8ea68edf7d7974b2c8b
QingMingZhang <laznrbfe@163.com>
2016-09-12 10:14:39 +0800

    更改“表情”按钮控件类名

---
+++ EmoticonsKeyboardSwift/EmoticonsKeyboardSwift/EmoticonsKeyboard/QMEmoticonButton.swift
@@ -0,0 +1,27 @@
+//
+//  QMEmoticonButton.swift
+//  EmoticonsKeyboardSwift
+//
+//  Created by QingMingZhang on 16/9/10.
+//  Copyright © 2016年 极客栈. All rights reserved.
+//
+
+import UIKit
+
+class QMEmoticonButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    var imageName: String = ""
+    var desc: String = ""
+    var category: String = ""
+    var isDelete: Bool = false
+    var isAdd: Bool = false
+
+}
