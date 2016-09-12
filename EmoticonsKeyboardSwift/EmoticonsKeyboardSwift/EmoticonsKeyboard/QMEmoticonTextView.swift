2172f25faf3a96778f2db8c834e20b2d5f501fad
QingMingZhang <laznrbfe@163.com>
2016-09-12 10:15:48 +0800

    自定义TextView

---
+++ EmoticonsKeyboardSwift/EmoticonsKeyboardSwift/EmoticonsKeyboard/QMEmoticonTextView.swift
@@ -0,0 +1,33 @@
+//
+//  QMEmoticonTextView.swift
+//  EmoticonsKeyboardSwift
+//
+//  Created by QingMingZhang on 16/9/10.
+//  Copyright © 2016年 极客栈. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol QMEmoticonTextViewDelegate: UITextViewDelegate {
+    optional func textViewDidDeleteBackward(textView: UITextView)
+}
+
+class QMEmoticonTextView: UITextView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    var emotionDelegate: QMEmoticonTextViewDelegate? = nil
+
+    override func deleteBackward() {
+        if false {
+            
+        }
+        super.deleteBackward()
+    }
+}
